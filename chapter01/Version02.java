6cf5cecf2f55cb2019816bb26bec05283dd0a1df
SangHunKim <kim199603330@gmail.com>
2025-11-26 12:09:56 +0900

    version control5_A

--- chapter01/Version02.java
+++ chapter01/Version02.java
@@ -1,5 +1,6 @@
 public class Version02 {
-    public void Version02(){
+
+    public Version02(){
         System.out.println("untracked 파일");
     }
 }
---
+++ chapter01/Version03.java
@@ -0,0 +1,7 @@
+public class Version03 {
+    
+    public Version03(){
+        System.out.println("커밋된적없음");
+    }
+
+}
