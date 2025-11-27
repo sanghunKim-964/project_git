c9978625280ef583b667ba064556d87c1c2de749
SangHunKim <kim199603330@gmail.com>
2025-11-27 11:44:13 +0900

    version5

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,11 +3,7 @@ package chapter02;
 public class Branch01 {
  
     public Branch01(){
-<<<<<<< HEAD
         System.out.println("메인 구글에서 작업");
-=======
-        System.out.println("머지 페일");
->>>>>>> google
         System.out.println("다음 브랜치에서 작업");
     }
 
